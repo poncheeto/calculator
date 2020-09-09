a9a120eb8058a59e1414d3e1c9f26881b84de65e
Poncho <jplyles1212@gmail.com>
2020-09-09 16:20:13 +0900

    Add operator functions to add, subtract, multiply, and divide

---
+++ main.js
@@ -0,0 +1,18 @@
+// Create functions to perform maths
+
+// Add
+const add = (num1, num2) => num1 + num2;
+
+// Subtract
+
+const subtract = (num1, num2) => num1 - num2;
+
+// Multiply 
+
+const multiply = (num1, num2) => num1 * num2;
+
+// Divide
+
+const divide = (num1, num2) => num1 / num2;
+
+//Create operate function 
